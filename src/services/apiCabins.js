1920bad04c0f2daf9fc40ea1cb2f05e69ef2ce8a
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-10-17 23:02:18 +0100

    Update: New cabin created

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,12 +1,34 @@
 import supabase from "./supabase";
 
 export async function getCabins() {
- const { data, error } = await supabase.from("Cabins").select("*");
+  const { data, error } = await supabase.from("Cabins").select("*");
 
- if(error)
- {
-  console.error(error)
-  throw new Error("Cabins could not be loaded")
- }
- return data;
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be loaded");
+  }
+  return data;
+}
+
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("Cabins")
+    .insert([newCabin])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be created");
+  }
+  return data;
+}
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("Cabins").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabin could not be deleted");
+  }
+  return data;
 }
