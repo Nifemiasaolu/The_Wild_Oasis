4d9c6912b5fb3a884878464c0f2824de3f0126bd
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-10-19 12:21:34 +0100

    Supabase Cabin API

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://vnaeyyaupmjblcsqxwkr.supabase.co";
+export const supabaseUrl = "https://vnaeyyaupmjblcsqxwkr.supabase.co";
 const supabaseKey = 
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZuYWV5eWF1cG1qYmxjc3F4d2tyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc0ODI5NDcsImV4cCI6MjAxMzA1ODk0N30.QsSLAdM4oJ2cpk3fWo23-qhG4tqTRxg2MVHjgnMqWlg";
 const supabase = createClient(supabaseUrl, supabaseKey);
