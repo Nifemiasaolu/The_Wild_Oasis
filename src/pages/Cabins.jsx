638043542ac587db5d2a5afef8d39ec171a088d9
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-10-23 14:28:11 +0100

    Update: Refactored contents

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -4,9 +4,9 @@ import CabinTable from "../features/cabins/CabinTable";
 import Button from "../ui/Button";
 import { useState } from "react";
 import CreateCabinForm from "../features/cabins/CreateCabinForm";
+import AddCabin from "../features/cabins/AddCabin";
 
 function Cabins() {
-  const [showForm, setShowForm] = useState(false);
   return (
     <>
       <Row type="horizontal">
@@ -17,10 +17,7 @@ function Cabins() {
       <Row>
         <CabinTable />
 
-        <Button onClick={() => setShowForm((show) => !show)}>
-          {showForm ? "Close Form" : "Add new cabin"}
-        </Button>
-        {showForm && <CreateCabinForm/>}
+        <AddCabin />
       </Row>
     </>
   );
