cd3db8fdd2dcbb95edd35d05af522770206427d1
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-10-25 15:01:36 +0100

    Update: Little tweak

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -9,7 +9,6 @@ import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
-import Button from "../../ui/Button";
 
 // const TableRow = styled.div`
 //   display: grid;
