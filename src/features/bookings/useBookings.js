c60b383c798477679c1ce343ba423fb970cec51d
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-10-26 14:45:34 +0100

    Filter logic on bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  // FILTER
+  const filteredValue = searchParams.get("status");
+  const filter =
+    !filteredValue || filteredValue === "all"
+      ? null
+      : { field: "status", value: filteredValue };
+
   const {
     data: bookings,
     isLoading,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings({ filter }),
   });
 
   return { bookings, isLoading, error };
