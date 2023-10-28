397ad2315573ac63f4b3fb87cea425a88662ce27
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-10-28 15:46:43 +0100

    FIxed very stubborn bug

--- src/features/cabins/useCreateCabin.js
+++ src/features/cabins/useCreateCabin.js
@@ -1,16 +1,16 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createEditCabin } from "../../services/apiCabins";
+import { createEditCabin as createEditCabinAPI } from "../../services/apiCabins";
 import toast from "react-hot-toast";
 
 export function useCreateCabin() {
   const queryClient = useQueryClient();
   const { mutate: createCabin, isLoading: isCreating } = useMutation({
     // mutationFn: createEditCabin, // Same as below
-    mutationFn: (newCabin) => createEditCabin(newCabin),
+    mutationFn: (newCabin) => createEditCabinAPI(newCabin),
     onSuccess: () => {
       toast.success("New cabin successfully added");
       queryClient.invalidateQueries({
-        queryKey: ["Cabins"],
+        queryKey: ["cabins"],
       });
     },
     onError: (err) => toast.error(err.message),
