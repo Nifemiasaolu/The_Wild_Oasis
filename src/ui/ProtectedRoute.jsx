6841448e86398814cae11f7c5e5ee8460fc4b5e7
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-01 21:53:53 +0100

    A little detail tweak

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -32,7 +32,7 @@ function ProtectedRoute({ children }) {
     );
 
   // 4) If there IS a user, render the app
-  if(isAuthenticated) return children;
+  if (isAuthenticated) return children;
 }
 
 export default ProtectedRoute;
