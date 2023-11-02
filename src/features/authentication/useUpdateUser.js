a694697f6f007b7218ef2cc522e499d7ef1d274b
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-02 11:49:11 +0100

    Update user hook

---
+++ src/features/authentication/useUpdateUser.js
@@ -0,0 +1,24 @@
+import toast from "react-hot-toast";
+import { createEditCabin } from "../../services/apiCabins";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateCurrentUser as updateCurrentUserAPI } from "../../services/apiAuth";
+
+export function useUpdateUser() {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateUser, isLoading: isUpdating } = useMutation({
+  // mutationFn: (id) => updateCurrentUserAPI(id), // Same as below
+  mutationFn: updateCurrentUserAPI,
+  onSuccess: ({user}) => {
+    toast.success("User account successfully updated");
+
+    // Updating data manually in the cache using queryClient.setQueryData
+    queryClient.setQueryData(["user"], user);
+
+  },
+  onError: (err) => toast.error(err.message),
+});
+
+return {isUpdating, updateUser}
+}
+
