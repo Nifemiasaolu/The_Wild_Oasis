abbcb6bc8ec64dfaf7c83bcc311e0cda75e7858f
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-04 22:48:26 +0100

    Dashbord presentation

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,8 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
+import Stats from "./Stats";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +10,21 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { bookings, isLoading: isBooking } = useRecentBookings();
+  const { stays, confirmedStays, isLoading: isStaying } = useRecentStays();
+
+  if (isBooking || isStaying) return <Spinner />;
+  // console.log(bookings);
+
+  return (
+    <StyledDashboardLayout>
+      <Stats bookings={bookings} confirmedStays={confirmedStays} />
+      <div>Today`s activity</div>
+      <div>Chart stay duration</div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
+export default DashboardLayout;
