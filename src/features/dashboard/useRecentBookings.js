227aa306694c53737be9a08aae223496a84d70c7
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-04 22:48:51 +0100

    Recent stays hook

---
+++ src/features/dashboard/useRecentBookings.js
@@ -0,0 +1,24 @@
+import { useQuery } from "@tanstack/react-query";
+import { subDays } from "date-fns";
+import { useSearchParams } from "react-router-dom";
+import { getBookingsAfterDate } from "../../services/apiBookings";
+
+export function useRecentBookings() {
+  const [searchParams] = useSearchParams();
+
+  const numDays = !searchParams.get("last")
+    ? 7
+    : Number(searchParams.get("last"));
+
+  // Calculate the number of days ago and convert to ISOString
+  // Use the "subDays" method to do this type of calculation
+  const queryDate = subDays(new Date(), numDays).toISOString();
+
+  // Use the function using React Query
+  const { isLoading, data: bookings } = useQuery({
+    queryFn: () => getBookingsAfterDate(queryDate),
+    queryKey: ["bookings", `last-${numDays}`],
+  });
+
+  return { isLoading, bookings };
+}
