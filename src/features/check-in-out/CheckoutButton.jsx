ce5523bc3c2a033f8a8f68fa5db92fff728af12d
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-11-06 22:16:48 +0100

    Functionalize checkout on dashboard

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,6 +1,9 @@
 import Button from "../../ui/Button";
+import { useCheckout } from "./useCheckout";
 
 function CheckoutButton({ bookingId }) {
+  const {checkout, isCheckingOut} = useCheckout();
+
   return (
     <Button variation="primary" size="small">
       Check out
