9395a1da12d98d1acf29c6bec77abe2eb291e2c0
Nifemi Asaolu <theophilusasaolu@gmail.com>
2023-12-30 16:07:59 +0100

    Change on header responsiveness

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,6 +1,21 @@
-import styled from "styled-components"
-import HeaderMenu from "./HeaderMenu"
-import UserAvatar from "../features/authentication/UserAvatar"
+import styled from "styled-components";
+import HeaderMenu from "./HeaderMenu";
+import UserAvatar from "../features/authentication/UserAvatar";
+import { FaTimes } from "react-icons/fa";
+import { CgMenuRight } from "react-icons/cg";
+
+const BigContainer = styled.div`
+
+@media screen and (max-width: 960px) {
+  
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  background-color: var(--color-grey-0);
+  /* vertical-align: middle; */
+}
+
+`;
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
@@ -11,15 +26,42 @@ const StyledHeader = styled.header`
   align-items: center;
   justify-content: end;
   gap: 2.4rem;
-`
 
-function Header() {
+  @media screen and (max-width: 768px) {
+    padding-right: 1rem;
+    /* padding-top: 0; */
+    gap: 0rem;
+  }
+`;
+
+const MobileIcon = styled.div`
+  display: none;
+  z-index: 50;
+
+  @media screen and (max-width: 960px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    transform: translate(100%, 60%);
+    font-size: 2rem;
+    cursor: pointer;
+  }
+`;
+
+function Header({ show, onClick }) {
   return (
-    <StyledHeader>
-      <UserAvatar/>
-      <HeaderMenu/>
-    </StyledHeader>
-  )
+    <BigContainer>
+      <MobileIcon onClick={onClick}>
+        {show ? <FaTimes /> : <CgMenuRight />}
+      </MobileIcon>
+
+      <StyledHeader>
+        <UserAvatar />
+        <HeaderMenu />
+      </StyledHeader>
+    </BigContainer>
+  );
 }
 
-export default Header
+export default Header;
