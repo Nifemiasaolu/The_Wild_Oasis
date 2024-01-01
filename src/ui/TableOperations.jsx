2bc41832d28cb2b29cb4aa1d9692f3a94efeb8b4
Nifemi Asaolu <theophilusasaolu@gmail.com>
2024-01-01 15:38:42 +0100

    Table operation activity

--- src/ui/TableOperations.jsx
+++ src/ui/TableOperations.jsx
@@ -4,6 +4,11 @@ const TableOperations = styled.div`
   display: flex;
   align-items: center;
   gap: 1.6rem;
+
+  @media screen and (max-width: 760px) {
+    flex-direction: column;
+    gap: 2rem;
+  }
 `;
 
 export default TableOperations;
