e35373e3b2f7bf480f8f76823b8ffdad54a93157
Nifemi Asaolu <theophilusasaolu@gmail.com>
2024-01-15 22:00:38 +0100

    tweak on details

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,19 +2,23 @@ import DashboardFilter from "../features/dashboard/DashboardFilter";
 import DashboardLayout from "../features/dashboard/DashboardLayout";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { Section } from "../styles/GlobalStyles";
 
 function Dashboard() {
   return (
     <>
+    <Section smpadding="0">
+
       <Row type="horizontal">
         <Heading as="h1">Dashboard</Heading>
         <DashboardFilter/>
       </Row>
 
       <DashboardLayout />
+    </Section>
     </>
   );
 }
 
 export default Dashboard;
-// ////
+// ///
